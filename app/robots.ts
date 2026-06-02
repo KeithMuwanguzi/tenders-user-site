bafca19289e00b046c8d07c4ae2a1684c7919669
tenderlab333-star <tenderlab333@gmail.com>
2026-06-02 19:53:40 +0100

    SEO: declare sitemap-tenders.xml in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,10 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://www.tenderlab.co.uk/sitemap.xml',
+    sitemap: [
+      'https://www.tenderlab.co.uk/sitemap.xml',
+      'https://www.tenderlab.co.uk/sitemap-tenders.xml',
+    ],
     host: 'https://www.tenderlab.co.uk',
   }
 }
