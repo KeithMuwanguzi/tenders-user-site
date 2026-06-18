b2653cdb14347fb36a43a5afecf474ae39453f9d
tenderlab333-star <tenderlab333@gmail.com>
2026-06-19 00:21:36 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: { userAgent: '*', allow: '/', disallow: ['/*?ref=', '/*?utm_', '/*?source='] },
     sitemap: [
       'https://www.tenderlab.co.uk/sitemap.xml',
       'https://www.tenderlab.co.uk/sitemap-tenders.xml',
